e7ecdb7a07ae63b0073e42f6df418083594d8167
TMT-program <kurinamis@gmail.com>
2025-08-03 18:47:47 +0900

    firebase.ts 本番用プッシュ

--- backend/src/firebase.ts
+++ backend/src/firebase.ts
@@ -1,50 +1,50 @@
 //デプロイ環境で動かす時
-// import admin from 'firebase-admin';
+import admin from 'firebase-admin';
 
-// // 環境変数から値を取得
-// const projectId = process.env.FIREBASE_PROJECT_ID;
-// const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-// let privateKey = process.env.FIREBASE_PRIVATE_KEY;
+// 環境変数から値を取得
+const projectId = process.env.FIREBASE_PROJECT_ID;
+const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+let privateKey = process.env.FIREBASE_PRIVATE_KEY;
 
-// // 必須項目が欠けていないか確認
-// if (!projectId || !clientEmail || !privateKey) {
-//   throw new Error('Firebase環境変数が不足しています');
-// }
+// 必須項目が欠けていないか確認
+if (!projectId || !clientEmail || !privateKey) {
+  throw new Error('Firebase環境変数が不足しています');
+}
 
-// // 改行コード（\n）を正しく解釈
-// privateKey = privateKey.replace(/\\n/g, '\n');
+// 改行コード（\n）を正しく解釈
+privateKey = privateKey.replace(/\\n/g, '\n');
 
-// // Firebase Admin SDK の初期化
-// if (!admin.apps.length) {
-//   admin.initializeApp({
-//     credential: admin.credential.cert({
-//       projectId,
-//       clientEmail,
-//       privateKey,
-//     }),
-//   });
-// }
+// Firebase Admin SDK の初期化
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId,
+      clientEmail,
+      privateKey,
+    }),
+  });
+}
 
-// export { admin };
+export { admin };
 
 
 //////////////////////////////
 
 
 // localで動かす時
-import admin from 'firebase-admin';
-import path from 'path';
-import { readFileSync } from 'fs';
+// import admin from 'firebase-admin';
+// import path from 'path';
+// import { readFileSync } from 'fs';
 
-// JSON を require で読み込む（型安全でないが確実）
-const serviceAccount = JSON.parse(
-  readFileSync(path.resolve(__dirname, 'serviceAccountKey.json'), 'utf8')
-);
+// // JSON を require で読み込む（型安全でないが確実）
+// const serviceAccount = JSON.parse(
+//   readFileSync(path.resolve(__dirname, 'serviceAccountKey.json'), 'utf8')
+// );
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+// }
 
-export { admin };
+// export { admin };
